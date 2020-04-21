f8341f273c10202fcfc9f520b4bd79b7179b3d74
lennartdeknikker <me@lenn4rt.com>
2020-04-21 09:45:31 +0200

    add geojsons

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+const fs = require('fs-extra');
+
+
+
+
+const headers = {
+  contentType: 'text/xml'
+}
+
+async function getStops() {
+  await fetch('http://data.ndovloket.nl/haltes/PassengerStopAssignmentExportCHB20200420030138.xml.gz', { method: 'GET', compress: false})
+  .then(res => res.text())
+  .then(body => fs.writeFile('test.xml', body, 'binary'))
+}
+
+getStops()
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
