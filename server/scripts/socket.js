3f9f5361f0855a7af01565d8415fdb99f3873a47
lennartdeknikker <me@lenn4rt.com>
2020-04-28 17:32:37 +0200

    create room states

--- server/scripts/socket.js
+++ server/scripts/socket.js
@@ -1,18 +1,43 @@
+const Utilities = require('./utilities')
+
 function socket(io) {
+    const availableRooms = []
 
     io.on('connection', function(socket){
         console.log('a user connected')
 
-        socket.on('join', function(roomName, user) {
-            socket.join(roomName)
-            io.to(roomName).emit('new user', user)
+        socket.on('join', async function(roomName, userName) {
+            if (!io.sockets.adapter.rooms[roomName]) {
+                const newRoom = await Utilities.createRoom(roomName)
+                availableRooms.push(newRoom)
+            }
 
-            console.log(user, 'joined', roomName)
-            console.log('room details', io.sockets.adapter.rooms[roomName])                       
+            const newUser = await Utilities.createUser(userName, socket.id)
+            const roomIndex = availableRooms.findIndex(room => room.roomName === roomName)
+            availableRooms[roomIndex].users.push(newUser)
+            availableRooms[roomIndex].userTotal++
+
+            socket.join(roomName)
+            io.to(roomName).emit('new user', userName)
+
+            console.log(userName, 'joined', roomName)
+            console.log('room details', io.sockets.adapter.rooms[roomName])    
+            console.log(availableRooms)
         })
 
         socket.on('disconnect', function() {
-            console.log('user disconnected')      
+            availableRooms.forEach(room => {
+                room.users.forEach(user => {
+                    if (user.id === socket.id) {
+                        const index = room.users.indexOf(user)
+                        room.users.splice(index, 1)
+                        room.userTotal--
+                    }
+                })
+            })
+            console.log('user disconnected', socket.id)
+            console.log(availableRooms)
+            
         })
         socket.on('wiki link clicked', link => {
             console.log(link)      
---
+++ server/scripts/utilities.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+
+const Utilities = {
+    createUser: function(username, id) {
+        return {
+            username: username,
+            id: id,
+            score: 0,
+            clicks: 0
+        }
+    },
+    getRandomWikiLinks: async function (amount = 3) {
+        let links = []
+        for (let i = 0; i < amount; i++) {
+            const response = await fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary')
+            const json = await response.json()
+            links.push(json.content_urls.desktop.page)
+        }
+        return links
+    },
+    createRoom: async function(roomName) {
+        let newRoom = {
+            roomName: roomName,
+            userTotal: 0,
+            users: [],
+            startLinks: [],
+            destinationLinks: []
+        }
+
+        newRoom.destinationLinks = await Utilities.getRandomWikiLinks(1)
+        newRoom.startLinks = await Utilities.getRandomWikiLinks(1)
+        return newRoom
+    }
+}
+
+module.exports = Utilities
